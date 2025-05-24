0eae57c911ff4ac50e130a39be18ca97e985d2af
KarishmaAbruk <138876092+KarishmaAbruk@users.noreply.github.com>
2025-05-24 17:28:45 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,4 @@
+region         = "us-east-1"
+ami_id         = "ami-0e58b56aa4d64231b"  # Example: Amazon Linux 2 AMI
+instance_type  = "t2.micro"
+key_name       = "Jenkindemo"            # Do not include .pem
